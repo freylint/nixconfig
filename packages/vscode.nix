4c56ee530d716c18af384efae3f08dc18a1e80e8
freylint <lmpriestley@tuta.io>
2023-02-25 20:37:56 -0500

    Add rust analyzer extension.

--- packages/vscode.nix
+++ packages/vscode.nix
@@ -31,6 +31,14 @@
           version = "0.16.4";
           sha256 = "8fe3f6a29ae91f4af3a88d152add096d91b5f440c4edeefe9006f73061824439";
         }
+        # Rust tooling
+        {
+          name = "rust-analyzer";
+          publisher = "rust-lang";
+          version = "0.4.1416";
+          sha256 = "c6hRhXczpZwZV/49y373nZZh/5lonW5SdhRwxzFxPcU=";
+        }
+
       ];
     })
   ];
